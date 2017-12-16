e8e70049f685d12c20109fa2c20803ba36e18194
DeckerSU <support@decker.su>
2017-12-16 05:16:49 +0300

    fixed start_generate script

--- start_generate.sh
+++ start_generate.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # node 1
-src/qt/vivo-qt -server=1 -daemon=1 -rpcuser=user -rpcpassword=password -addnode=127.0.0.1 & 
+#testnet=-testnet
+testnet=
+src/qt/vivo-qt $testnet -server=1 -daemon=1 -rpcuser=user -rpcpassword=password -datadir=/home/decker/ssd/StolypinCoin/node1 & 
 # node 2
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node2 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node3 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node4 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node5 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node6 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node7 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node8 -listen=0 &
-src/qt/vivo-qt -addnode=127.0.0.1 -datadir=/home/decker/ssd/StolypinCoin/node9 -listen=0 -gen=1 -genproclimit=8 &
+src/qt/vivo-qt $testnet -addnode=127.0.0.1:12845 -datadir=/home/decker/ssd/StolypinCoin/node2 -listen=0  -gen=1 -genproclimit=8 &
